fc2eac636e54e02b3e9c786401f6b92e92822b5a
JAMIL RAYHAN <85067348+rjamil336@users.noreply.github.com>
2021-06-30 18:31:55 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+  
+ function togglemenu(){
+    let menu = document.querySelector('#menu');
+
+
+     if(menu.style.display == 'block'){
+         menu.style.display = 'none';
+     }
+     else{
+        menu.style.display = 'block';
+     }
+
+ }
+
+
+
+
+
